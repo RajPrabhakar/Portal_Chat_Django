4dcc00f65509b1a1b3f4d9f72bccbd258c93d5f6
RajPrabhakar <rajprabhakar1804@gmail.com>
2021-05-25 02:05:11 +0530

    Resolved Key Issue

--- portal/settings.py
+++ portal/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from .key import Secret
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ojs^28k%3-+x3#2#-%jdp-qjv=c2&9%k#m-f_k=@t(xyy3!crp'
+SECRET_KEY = Secret.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
